7fa1ac0581054deb5dfa73ab44cd1f3337f9b07f
Satvik-N <34355902+Satvik-N@users.noreply.github.com>
2019-02-05 13:35:43 -0800

    Create CalibrateTilt.java
    Created new CalibrateTilt file

---
+++ src/main/java/frc/robot/commands/CalibrateTilt.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+class CalibrateTilt extends Command
+{
+   CalibrateTilt()
+   {
+   }
+
+   @Override
+   protected void initialize()
+   {
+   }
+   
+   @Override
+   protected void execute()
+   {
+   }
+   
+   @Override
+   protected boolean isFinished()
+   {
+      return true;
+   }
+   
+   @Override 
+   protected void end()
+   {
+   }
+   
+   void calibrate()
+   {
+   }
+   
+   @Override
+   protected void interrupted()
+   {
+   }
+}
