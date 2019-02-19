59a6327bcf114a29b721865768657081c6ab6299
Pranav Patil <4pranavpatil@gmail.com>
2019-02-19 07:19:04 -0800

    Simplify Unload command

--- src/main/java/frc/robot/commands/Unload.java
+++ src/main/java/frc/robot/commands/Unload.java
@@ -5,39 +5,26 @@ import frc.robot.Robot;
 
 public class Unload extends Command
 {
-	private long endTime;
 	public Unload()
 	{
 		requires(Robot.getFlywheel());
-		endTime = -1;
 	}	
 	
 	protected void initialize(){}
 
 	protected void execute()
 	{
-		System.out.println(Robot.getFlywheel().hasCargo());
 		Robot.getFlywheel().unload();
-		if (!Robot.getFlywheel().hasCargo())
-			endTime = System.currentTimeMillis();
 	}
 
 	protected boolean isFinished()
 	{
-		if (endTime == -1)
-		{
-			return false;
-		}
-		else
-		{
-			return System.currentTimeMillis() - endTime > 500;
-		}
+		return false;
 	}
 
 	protected void end() 
 	{
 		Robot.getFlywheel().setSpeed(0);
-		endTime = -1;
 	}	
 
 	protected void interrupted() 
