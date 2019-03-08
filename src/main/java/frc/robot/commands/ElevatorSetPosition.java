38fd86e3502cb5416ecb4f887f2aa8ae652faf0b
AndrewLiu7127 <42665200+AndrewLiu7127@users.noreply.github.com>
2019-03-07 21:25:42 -0800

    a

--- src/main/java/frc/robot/commands/ElevatorSetPosition.java
+++ src/main/java/frc/robot/commands/ElevatorSetPosition.java
@@ -1,22 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class ElevatorSetPosition extends Command {
-   private int position;
+public class ElevatorStick extends Command {
+  public ElevatorStick() {
+    requires(Robot.getElevator());
+  }
 
-   public ElevatorSetPosition(int pos) {
-        requires(Robot.getElevator());
-        position = pos;
-   }
-    @Override 
-    protected void execute() {
-        Robot.getElevator().setPosition(position);
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.getElevator().setVelocity(Robot.getOI().getElevatorY()); 
+    if (Robot.getMap().getSys("elevator") == 2)
+    {
+      System.out.println("Elevator Position: " + Robot.getElevator().getPosition());
+      System.out.println("Elevator Setpoint: " + Robot.getElevator().getSetpoint());
+      System.out.println("Elevator Y: " + Robot.getOI().getElevatorY());
     }
+  }
 
-    @Override
-    protected boolean isFinished() {
-        return true;
-    }
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 }
